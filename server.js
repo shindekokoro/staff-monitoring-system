d9ba7b08be9b30e27f2a3d0460c339a9cad9fbdd
Brian Whisler <brian.whisler@gmail.com>
2023-08-26 17:05:41 -0600

    server js for potential frontend

---
+++ server.js
@@ -0,0 +1,7 @@
+// TODO: Start Creating an express server to host an interactive frontend
+
+const config = require('./config/config')
+const sms = require('./modules/index.js');
+
+
+const express = require('express');
