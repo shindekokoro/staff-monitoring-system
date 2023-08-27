577f5a405f315df0f7e49cc1b44b9813e78d197c
Brian Whisler <brian.whisler@gmail.com>
2023-08-27 15:38:20 -0600

    add table output formatting

--- modules/index.js
+++ modules/index.js
@@ -1,51 +1,59 @@
 const appLogo = "              _____ _         __  __               \r\n             \/ ____| |       \/ _|\/ _|              \r\n            | (___ | |_ __ _| |_| |_               \r\n             \\___ \\| __\/ _` |  _|  _|              \r\n             ____) | || (_| | | | |                \r\n  __  __    |_____\/ \\__\\__,_|_| |_|  _             \r\n |  \\\/  |           (_) |           (_)            \r\n | \\  \/ | ___  _ __  _| |_ ___  _ __ _ _ __   __ _ \r\n | |\\\/| |\/ _ \\| \'_ \\| | __\/ _ \\| \'__| | \'_ \\ \/ _` |\r\n | |  | | (_) | | | | | || (_) | |  | | | | | (_| |\r\n |_|  |_|\\___\/|_| |_|_|\\__\\___\/|_|  |_|_| |_|\\__, |\r\n        \/ ____|         | |                   __\/ |\r\n       | (___  _   _ ___| |_ ___ _ __ ___    |___\/ \r\n        \\___ \\| | | \/ __| __\/ _ \\ \'_ ` _ \\         \r\n        ____) | |_| \\__ \\ ||  __\/ | | | | |        \r\n       |_____\/ \\__, |___\/\\__\\___|_| |_| |_|        \r\n                __\/ |                              \r\n               |___\/\n";
 const inquirer = require('inquirer');
-const config = require('../config/config.json');
+const { printTable } = require('console-table-printer')
+const colors = require('colors')
 
-const questions = require('./questions');
+const config = require('../config/config.json')
+const questions = require('./questions').questions;
 const actions = require('./actions');
 
+colors.enable()
+
 async function init() {
+    // Display Name of App/LOGO
     console.log(appLogo);
     return await inquirer
-        .prompt(questions.actions)
+        .prompt(questions)
         .then(async (answers) => {
-            console.log(answers);
+            config.logging.enabled ? console.log(answers) : '';
             // Actions Switch
             switch (answers.action) {
                 case 'viewDepartments':
-                    actions.view('department');
+                    printTable(await actions.view('department'))
                     break;
                 case 'viewRoles':
-                    actions.view('role');
+                    printTable(await actions.view('role'))
                     break;
                 case 'viewEmployees':
-                    actions.view('employee');
+                    printTable(await actions.view('employee'))
                     break;
                 case 'addDepartment':
                     actions.addDepartment(answers.departmentName);
                     break;
                 case 'addRole':
-                    actions.addRole(answers.roleName);
+                    let salary = parseFloat(answers.salary).toFixed(2);
+                    let department_id = parseInt(answers.department_id);
+                    actions.addRole(answers.roleName, salary, department_id);
                     break;
                 case 'addEmployee':
-                    actions.addEmployee(answers.employeeName)
+                    actions.addEmployee(answers.first_name, answers.last_name, answers.role_id, answers.manager);
                     break;
                 case 'updateRole':
+                    actions.updateEmployeeRole(answers.employee_id, answers.role_id);
                     break;
                 case 'quit':
-                    console.log('User has requested to quit application.');
+                    console.log('User has requested to quit application.'.blue);
                     return process.exit()
                 default:
-                    throw new Error(`${answers.action} has not been accounted for, submit and issue on GitHub`)
+                    throw new Error(`${answers.action} has not been accounted for, submit an issue on GitHub`)
             }
             return await init();
         })
         .catch((error) => {
             if (error.isTtyError) {
-                console.error('Prompt cannot be rendered in the current environment.');
+                console.error('Prompt cannot be rendered in the current environment.'.red);
             } else {
-                console.error('Something went wrong %s', error);
+                console.error('Something went wrong %s'.red, error);
             }
         });
 }
