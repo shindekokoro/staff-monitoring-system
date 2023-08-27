b8dd2387332bb0e7605650e0ad3008bb59e11a67
Brian Whisler <brian.whisler@gmail.com>
2023-08-27 17:08:47 -0600

    formatting for readability

--- modules/actions.js
+++ modules/actions.js
@@ -1,8 +1,17 @@
 const database = require('./database');
 
+const formatName = (input) => {
+    return input.charAt(0).toUpperCase() + input.slice(1).toLowerCase();
+}
+
 const view = (table, id) => {
     let sql = `SELECT * FROM \`${table}\`${id ? 'WHERE `id` = ?' : ''}`;
-    return database.sqlFunction(sql, id, `Successfully queried ${table}`, `Unable to pull '${table}' records.`)
+    return database.sqlFunction(
+        sql,
+        id,
+        `Successfully queried ${table}`,
+        `Unable to pull '${table}' records.`
+    );
 }
 
 const getDepartmentsList = async () => {
@@ -34,13 +43,23 @@ const getEmployeesList = async () => {
 const addDepartment = (name) => {
     if (!name) { return 'No department name passed.'; }
     let sql = 'INSERT INTO `department` (name) VALUES (?);'
-    database.sqlFunction(sql, name, `${name} added to department table.`, `Unable to add ${name} department to table.`);
+    database.sqlFunction(
+        sql,
+        formatName(name),
+        `${name} added to department table.`,
+        `Unable to add ${name} department to table.`
+    );
 }
 
 const addRole = (title, salary, department_id) => {
     if (!title) { return 'No role title passed.'; }
     let sql = 'INSERT INTO `role` (title, salary, department_id) VALUES (?, ?, ?);';
-    database.sqlFunction(sql, [title, salary, department_id], `${title} added to role table.`, `Unable to add '${title}' department to table.`)
+    database.sqlFunction(
+        sql,
+        [formatName(title), salary, department_id],
+        `${title} added to role table.`,
+        `Unable to add '${title}' department to table.`
+    )
         .catch(error => {
             if (error.code === 'ER_DUP_ENTRY') {
                 console.warn(`${title} is already a role in your selected department. Try a again, or try a new role.`);
@@ -48,20 +67,28 @@ const addRole = (title, salary, department_id) => {
             else {
                 console.error(`${error}`);
             }
-        });;
+        });
 }
 
 const addEmployee = (first_name, last_name, role_id, manager_id) => {
-    if (!first_name) { return 'No first name passed.'; }
-    if (!last_name) { return 'No last name passed.'; }
     let sql = 'INSERT INTO `employee` (first_name, last_name, role_id, manager_id) VALUES (?, ?, ? ,?);';
-    database.sqlFunction(sql, [first_name, last_name, role_id, manager_id], `${first_name} ${last_name} added to employee table.`, `Unable to update employee.`);
+    database.sqlFunction(
+        sql,
+        [formatName(first_name), formatName(last_name), role_id, manager_id],
+        `${first_name} ${last_name} added to employee table.`,
+        `Unable to update employee.`
+    );
 }
 
 const updateEmployeeRole = (employee_id, role_id) => {
     if (!employee_id || !role_id) { return 'Needed IDs not provided, please try again'; }
     let sql = 'UPDATE `employee` SET `role_id` = ? WHERE `id` = ?';
-    database.sqlFunction(sql, [role_id, employee_id], `Successfully updated employee's role.`, `Unable to update employee`)
+    database.sqlFunction(
+        sql,
+        [role_id, employee_id],
+        `Successfully updated employee's role.`,
+        `Unable to update employee`
+    );
 }
 
 module.exports = { view, getDepartmentsList, getRolesList, getEmployeesList, addDepartment, addRole, addEmployee, updateEmployeeRole }
