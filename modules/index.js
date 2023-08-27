f81301b61546ee49a2f90ac61682163fdf88685c
Brian Whisler <brian.whisler@gmail.com>
2023-08-26 18:12:38 -0600

    add more switch cases and catch error

--- modules/index.js
+++ modules/index.js
@@ -3,34 +3,47 @@ const Inquirer = require('inquirer')
 const config = require('../config/config.json');
 
 const questions = require('./questions');
+const actions = require('./actions');
 
-Inquirer
-    .prompt(questions.actions)
-    .then((answers) => {
-        console.log(answers);
-        // Actions Switch
-        switch (answers.action) {
-            case 'viewDepartments':
-                //formatted table showing department names and department ids
-                viewDepartments
-                break;
-            case 'viewRoles':
-                //job title, role id, the department that role belongs to, and the salary for that role
+async function init() {
+    return await Inquirer
+        .prompt(questions.actions)
+        .then(async (answers) => {
+            console.log(answers);
+            // Actions Switch
+            switch (answers.action) {
+                case 'viewDepartments':
+                    //formatted table showing department names and department ids
+                    break;
+                case 'viewRoles':
+                    //job title, role id, the department that role belongs to, and the salary for that role
 
-                viewRoles
-                break;
-            case 'viewEmployees':
-                //formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
-                viewEmployees
-                break;
-            default:
-                throw new Error(`Given answer ${answers.action} not a valid choice`);
-        }
-    })
-    .catch((error) => {
-        if (error.isTtyError) {
-            console.error('Prompt cannot be rendered in the current environment.');
-        } else {
-            console.error('Something went wrong %s', error);
-        }
-    });
+                    break;
+                case 'viewEmployees':
+                    //formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
+                    break;
+                case 'addDepartment':
+                    break;
+                case 'addRole':
+                    break;
+                case 'addEmployee':
+                    break;
+                case 'updateRole':
+                    break;
+                case 'quit':
+                    return console.log('User has requested to quit application.');
+                default:
+                    throw new Error(`${answers.action} has not been accounted for, submit and issue on GitHub`)
+            }
+            return await init();
+        })
+        .catch((error) => {
+            if (error.isTtyError) {
+                console.error('Prompt cannot be rendered in the current environment.');
+            } else {
+                console.error('Something went wrong %s', error);
+            }
+        });
+}
+
+init()
