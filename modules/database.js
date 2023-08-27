799f9a5670d39ad48389fe20ea5934f84a260e4c
Brian Whisler <brian.whisler@gmail.com>
2023-08-27 15:39:10 -0600

    config logging

--- modules/database.js
+++ modules/database.js
@@ -1,11 +1,12 @@
 const config = require('../config/config.json')
 const mysql = require('mysql2');
+const colors = require('colors');
 const fs = require('fs');
 const path = require('path');
-const { error } = require('console');
+
+colors.enable();
 
 const seedSql = fs.readFileSync(path.join(__dirname, '../static/database.sql')).toString();
-
 const smsDB = mysql.createConnection({
     host: config.db.host,
     port: config.db.port,
@@ -19,15 +20,17 @@ function startDB() {
     sqlFunction(seedSql, null, 'Database tables loaded/created successfully', 'Unable to load database tables.');
 }
 
-const sqlFunction = (sql, data, logSuccess, logError) => {
+const sqlFunction = async (sql, values, logSuccess, logError) => {
     return new Promise((resolve, reject) => {
-        smsDB.query(sql, data, function (error, result, fields) {
+        config.logging.enabled ? console.log('The query is: ' + sql) : '';
+        config.logging.enabled ? console.log('The values are: ' + values) : '';
+        smsDB.query(sql, values, function (error, result, fields) {
             if (error) {
-                console.error(logError);
+                console.error(`\n${logError}`.red);
                 reject(error)
             }
-            console.log(logSuccess);
-            resolve(result);
+            // console.log(logSuccess);
+            return resolve(result)
         });
     });
 }
