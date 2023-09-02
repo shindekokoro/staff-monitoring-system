0e7250f2c53c1b37ebf31e49924630231d82f6af
Brian Whisler <brian.whisler@gmail.com>
2023-09-02 06:59:16 -0600

    new view functions

--- modules/view.js
+++ modules/view.js
@@ -2,8 +2,12 @@ const sqlFunction = require('./database').sqlFunction;
 const { printTable } = require('console-table-printer');
 
 // View specified table and if there is an ID the specific entry.
-const getTable = (table, id) => {
-    let sql = `SELECT * FROM \`${table}\`${id ? 'WHERE `id` = ?' : ''}`;
+const getTable = (table, column, id) => {
+    let where;
+    if (column) {
+        where = `WHERE \`${column}\` = ?`
+    }
+    let sql = `SELECT * FROM \`${table}\` ${where}`;
     return sqlFunction(
         sql,
         id,
@@ -11,55 +15,105 @@ const getTable = (table, id) => {
         `Unable to pull '${table}' records.`
     );
 }
+const departmentMap = (department) => {
+    return {
+        'ID': department.id,
+        'Name': department.name,
+    }
+}
+const viewDepartments = async () => {
+    let departments = await getTable('department');
+    let departmentsTable = departments.map(departmentMap)
+
+    return printTable(departmentsTable)
+}
+const roleMap = (role, roleDepartments) => {
+    let currentDepartment = roleDepartments.find(department => department.id === role.department_id)
+    return {
+        'ID': role.id,
+        'Title': role.title,
+        'Salary': role.salary,
+        'Department': currentDepartment.name
+    }
+}
+const viewRoles = async () => {
+    let roles = await getTable('role');
+    let roleDepartments = await getTable('department');
+    let rolesTable = roles.map(role => roleMap(role, roleDepartments))
+    return printTable(rolesTable)
+}
+
+const employeeMap = (employee, employeesList, employeeRoles) => {
+    let currentRole = employeeRoles.find(role => role.id === employee.role_id);
+    let currentManager = employeesList.find((manager) => {
+        if (employee.manager_id === manager.id) { return manager }
+        if (employee.manager_id === null) { return null }
+    });
+    let managerName = currentManager ? currentManager.first_name + ' ' + currentManager.last_name : 'none';
+    return {
+        'ID': employee.id,
+        'First Name': employee.first_name,
+        'Last Name': employee.last_name,
+        'Department Role': currentRole.title,
+        'Manager': managerName
+    }
+}
+const viewEmployees = async () => {
+    let employeesList = await getTable('employee');
+    let employeeRoles = await getTable('role');
+    let employeesTable = employeesList.map((employee) => employeeMap(employee, employeesList, employeeRoles))
+    return printTable(employeesTable)
+}
+const viewEmployeeByManager = async (manager_id) => {
+    let employeesList = await getTable('employee', 'manager_id', manager_id)
+    let employeeRoles = await getTable('role');
+    if (!employeesList || employeesList.length < 1) {
+        return console.log('Manager has no subordinates.');
+    }
+    let employeesTable = employeesList.map(employee => employeeMap(employee, employeesList, employeeRoles))
+    return printTable(employeesTable)
+}
+const viewEmployeeByDepartment = async (department_id) => {
+    let employeesList = await getTable('employee',);
+    let departmentRoles = await getTable('role', 'department_id', department_id);
+    let roleIDs = departmentRoles.map(department => department.id)
+    let departmentEmployees = employeesList.filter(employee => {
+        if (roleIDs.indexOf(employee.role_id) > -1) {
+            return employee
+        }
+    });
+    let employeesTable = departmentEmployees.map(employee => employeeMap(employee, departmentEmployees, departmentRoles));
+    return printTable(employeesTable)
+}
+
+const viewDepartmentBudget = async () => {
+    let department = await getTable('department')
+}
 
 const view = async (questions, answers) => {
     // Actions Switch
     switch (answers.action) {
         case 'departments':
-            let viewDepartments = await getTable('department');
-            let departmentsTable = viewDepartments.map((department) => {
-                return {
-                    'ID': department.id,
-                    'Name': department.name,
-                }
-            })
-
-            return printTable(departmentsTable)
+            return await viewDepartments();
         case 'roles':
-            let viewRoles = await getTable('role');
-            let roleDepartments = await getTable('department');
-            let rolesTable = viewRoles.map((role) => {
-                let currentDepartment = roleDepartments.find(department => department.id === role.department_id)
-                return {
-                    'ID': role.id,
-                    'Title': role.title,
-                    'Salary': role.salary,
-                    'Department': currentDepartment.name
-                }
-            })
-            return printTable(rolesTable)
+            return await viewRoles();
         case 'employees':
-            let viewEmployees = await getTable('employee');
-            let employeeRoles = await getTable('role');
-            let employeesTable = viewEmployees.map((employee) => {
-                let currentRole = employeeRoles.find(role => role.id === employee.role_id);
-                let currentManager = viewEmployees.find((manager) => {
-                    if (employee.manager_id === manager.id) { return manager }
-                    if (employee.manager_id === null) { return null }
-                });
-                let managerName = currentManager ? currentManager.first_name + ' ' + currentManager.last_name : 'none';
-                return {
-                    'ID': employee.id,
-                    'First Name': employee.first_name,
-                    'Last Name': employee.last_name,
-                    'Department Role': currentRole.title,
-                    'Manager': managerName
-                }
-            })
-            return printTable(employeesTable)
+            return await viewEmployees();
         case 'employeeManager':
-            console.log('Add Stuff Here');
-            break;
+            return await questions.inquirer
+                .prompt(questions.viewEmployeeManager)
+                .then(async (employee) => {
+                    return await viewEmployeeByManager(employee.manager_id);
+                });
+        case 'employeeDepartment':
+            return await questions.inquirer
+                .prompt(questions.viewEmployeeDepartment)
+                .then(async (employee) => {
+                    return await viewEmployeeByDepartment(employee.department_id);
+                });
+
+        case 'budget':
+            return await viewDepartmentBudget(answers.department_id);
         case 'goBack':
             return console.log('Going Back');
         default:
